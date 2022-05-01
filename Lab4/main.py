4c7f4af080efa16223292fc2c2aa512e4887fac8
Jairo Céspedes <ja.cespedes@uniandes.edu.co>
2022-05-01 15:06:40 -0500

    Update main.py

--- Lab4/main.py
+++ Lab4/main.py
@@ -1,7 +1,39 @@
 from typing import Optional
-
+from joblib import load
 from fastapi import FastAPI
 
+#import DataModel
+
+import pandas as pd
+from pydantic import BaseModel
+
+class DataModel(BaseModel):
+
+# Estas varibles permiten que la librería pydantic haga el parseo entre el Json recibido y el modelo declarado.
+    adult_mortality: float
+    infant_deaths: float
+    alcohol: float
+    percentage_expenditure: float
+    hepatitis_B: float
+    measles: float
+    bmi: float
+    under_five_deaths: float
+    polio: float
+    total_expenditure: float
+    diphtheria: float
+    hiv_aids: float
+    gdp: float
+    population: float
+    thinness_10_19_years: float
+    thinness_5_9_years: float
+    income_composition_of_resources	: float
+    schooling: float
+
+#Esta función retorna los nombres de las columnas correspondientes con el modelo esxportado en joblib.
+    def columns(self):
+        return ["Adult Mortality", "infant deaths", "Alcohol","percentage expenditure","Hepatitis B", "Measles", "BMI",
+                "under-five deaths", "Polio", "Total expenditure", "Diphtheria", "HIV/AIDS", "DGP", "Population",
+                "thinness 10-19 years", "thinness 5-9 years", "Income composition of resources", "Schooling"]
 app = FastAPI()
 
 
@@ -13,3 +45,11 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
    return {"item_id": item_id, "q": q}
+
+@app.post("/predict")
+def make_predictions(dataModel: DataModel):
+    df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
+    df.columns = dataModel.columns()
+    model = load("assets/modelo.joblib")
+    result = model.predict(df)
+    return result
