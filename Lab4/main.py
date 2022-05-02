ed794f653359cc4e5c36399bf7f2d6df9850aa59
Jairo Céspedes <ja.cespedes@uniandes.edu.co>
2022-05-01 20:19:16 -0500

    Solo falta el README

--- Lab4/main.py
+++ Lab4/main.py
@@ -69,16 +69,6 @@ class DataLista(BaseModel):
    dataL: List[DataModelWithY]
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-   return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-   return {"item_id": item_id, "q": q}
-
 @app.post("/predict")
 def make_predictions(dataModel: DataModel):
     df = pd.DataFrame(dataModel.dict(), index=[0])
